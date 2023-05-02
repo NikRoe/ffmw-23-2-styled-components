d49e9e0fee934a0dc950c4f5ff23e8a675d168f1
Niklas Röttger <niklas.roettger@outlook.de>
2023-05-02 15:07:24 +0200

    add session code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
+import Button from "../components/Button";
+import Container from "../components/Container";
+import InfoBox from "../components/InfoBox";
+import Link from "../components/Link";
+
 export default function HomePage() {
   return (
     <>
-      <h1>Hello from Next.js</h1>
+      <Container direction="column">
+        <InfoBox title="This is an info" message="Read it carefully!" />
+        <InfoBox
+          title="It worked"
+          message="You have done it"
+          variant="success"
+        />
+        <InfoBox title="Error" message="Something went wrong" variant="error" />
+      </Container>
+      <Container direction="column">
+        <Button>Click me</Button>
+        <Button color="danger">Click me not</Button>
+      </Container>
+      <Container>
+        <p>
+          This is a <Link href="/"> styled link</Link>.
+        </p>
+      </Container>
     </>
   );
 }
